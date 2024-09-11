ed6f9b8ae0d859816ae1f59924e5bca2b0b85c07
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-09-11 23:41:10 +0530

    [Jayesh Patil] add: This file checke first and last name and email and mobile validation.

--- user_registration.py
+++ user_registration.py
@@ -58,18 +58,40 @@ def is_valid_email(email):
         logger.error(f"Invalid email: {email}. Email should follow the format abc.xyz@bl.co.in")
         return False
 
+def is_valid_mobile(mobile):
+    """
+        Description:
+            Validates if the mobile number follows the format '91 9919819801'.
+            - It must start with a 2-digit country code.
+            - There should be a space after the country code.
+            - It should be followed by a 10-digit mobile number.
+        Parameters:  
+            mobile (str): The mobile input by the user.    
+        Returns:
+            True if the name is valid, False otherwise.      
+    """
+    pattern = r"^\d{2}\s\d{10}$"
+    if re.match(pattern, mobile):
+        logger.info(f"Valid mobile number entered: {mobile}")
+        return True
+    else:
+        logger.error(f"Invalid mobile number: {mobile}. It should follow the format '91 9919819801'.")
+        return False
 
 def main():
     first_name = input("Enter your first name: ")
     last_name = input("Enter your last name: ")
     email = input("Enter your email: ")
+    mobile = input("Enter your mobile number (e.g., 91 9919819801): ")
     
     valid_first_name = is_valid_name(first_name,"first")
     valid_last_name = is_valid_name(last_name,"Last")
     valid_email = is_valid_email(email)
+    valid_mobile = is_valid_mobile(mobile)
+
     
-    if valid_first_name and valid_last_name and valid_email:
-        logger.info(f"User successfully registered with First Name: {first_name} and Last Name: {last_name} and email : {email}")
+    if valid_first_name and valid_last_name and valid_email and valid_mobile:
+        logger.info(f"User successfully registered with First Name: {first_name}, and Last Name: {last_name}, and email : {email}, and Mobile: {mobile}")
     else:
         logger.error(f"Registration failed due to invalid inputs.")    
 
