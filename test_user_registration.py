d1c160f2a270a87031f19d141f2af5f7fbe12767
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-09-11 23:29:42 +0530

    [Jayesh Patil] add: This file use to test first and last name and email

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,6 +1,5 @@
 import unittest
-import user_registration 
-from user_registration import is_valid_name
+from user_registration import is_valid_name ,is_valid_email
 
 class TestNameValidation(unittest.TestCase):
 
@@ -12,7 +11,24 @@ class TestNameValidation(unittest.TestCase):
     def test_invalid_name_lowercase(self):
         """Test a name that doesn't start with a capital letter."""
         self.assertFalse(is_valid_name("jayesh","first"))
-        self.assertFalse(is_valid_name("patil","last")) 
+        self.assertFalse(is_valid_name("patil","last"))
+
+    def test_valid_email(self):
+        """Test a valid email format."""
+        self.assertTrue(is_valid_email("abc.xyz@bl.co.in"))
+
+    def test_invalid_email_missing_at(self):
+        """Test an email missing the @ symbol."""
+        self.assertFalse(is_valid_email("abc.xyzbl.co.in"))
+
+    def test_invalid_email_missing_domain(self):
+        """Test an email with no domain part after @."""
+        self.assertFalse(is_valid_email("abc@.co.in"))
+
+    def test_invalid_email_extra_dot(self):
+        """Test an email with an extra dot."""
+        self.assertFalse(is_valid_email("abc..xyz@bl.co.in"))
+     
 
       
 
