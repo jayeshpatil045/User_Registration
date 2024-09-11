74cdac79e8b66347b5cf7eb3e22ed156a988d399
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-09-11 22:46:03 +0530

    [Jayesh Patil] add: This file use to test first and last name

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,15 +1,20 @@
 import unittest
 import user_registration 
+from user_registration import is_valid_name
 
 class TestNameValidation(unittest.TestCase):
 
     def test_valid_name(self):
         """Test a valid name that starts with a capital letter and has at least 3 characters."""
-        self.assertTrue(is_valid_first_name("Jayesh"))
+        self.assertTrue(is_valid_name("Jayesh","first"))
+        self.assertTrue(is_valid_name("Patil","last"))
 
     def test_invalid_name_lowercase(self):
         """Test a name that doesn't start with a capital letter."""
-        self.assertFalse(is_valid_first_name("jayesh"))
+        self.assertFalse(is_valid_name("jayesh","first"))
+        self.assertFalse(is_valid_name("patil","last")) 
+
+      
 
 
 if __name__ == "__main__":
