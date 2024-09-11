bd6c60f9fb9ec9e47598b16d615f6f0354654f4d
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-09-11 23:43:01 +0530

    [Jayesh Patil] add: This file use to test first and last name and email and moblie

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,5 +1,5 @@
 import unittest
-from user_registration import is_valid_name ,is_valid_email
+from user_registration import is_valid_name ,is_valid_email,is_valid_mobile
 
 class TestNameValidation(unittest.TestCase):
 
@@ -29,7 +29,21 @@ class TestNameValidation(unittest.TestCase):
         """Test an email with an extra dot."""
         self.assertFalse(is_valid_email("abc..xyz@bl.co.in"))
      
+    def test_valid_mobile(self):
+        """Test a valid mobile number format."""
+        self.assertTrue(is_valid_mobile("91 9919819801"))
 
+    def test_invalid_mobile_no_country_code(self):
+        """Test an invalid mobile number without the country code."""
+        self.assertFalse(is_valid_mobile("9919819801"))
+
+    def test_invalid_mobile_no_space(self):
+        """Test an invalid mobile number without space after country code."""
+        self.assertFalse(is_valid_mobile("919919819801"))
+
+    def test_invalid_mobile_too_short(self):
+        """Test an invalid mobile number with less than 10 digits."""
+        self.assertFalse(is_valid_mobile("91 99198198"))
       
 
 
