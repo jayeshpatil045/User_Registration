6b35b16a92e3ef59bbfbd780c4415586d735ca2d
jayeshpatil045 <jaypatil04012003@gmail.com>
2024-09-11 22:43:04 +0530

    [Jayesh Patil] add: This file checke first and last name validation.

--- user_registration.py
+++ user_registration.py
@@ -13,32 +13,43 @@ import logger
 
 logger = logger.logger_init('user_registration')
 
-def is_valid_first_name(first_name):
+def is_valid_name(name,name_type):
     """
         Description:
-            Validates if the given first name starts with a capital letter and has a minimum of 3 characters.
+            Validates if the given first name and last name starts with a capital letter and has a minimum of 3 characters.
     
             The function uses a regular expression to ensure that the name:
             - Starts with an uppercase letter (A-Z).
             - Contains at least 3 alphabetic characters.
 
         Parameters:
-            name (str): The first name input by the user.
+            name (str): The name (first or last) input by the user.
+            name_type (str): Specifies whether it's a first name or last name for logging purposes.
 
         Returns:
             True if the name is valid, False otherwise.
     """
     pattern = r"^[A-Z][a-zA-Z]{2,}$"
-    if re.match(pattern, first_name):
-        logger.info(f"Valid first name entered: {first_name}")
+    if re.match(pattern, name):
+        logger.info(f"Valid {name_type} name entered: {name}")
         return True
     else:
-        logger.error(f"Invalid first name: {first_name}. It should start with a capital letter and be at least 3 characters long.")
+        logger.error(f"Invalid {name_type}: {name}. It should start with a capital letter and be at least 3 characters long.")
         return False
 
 def main():
     first_name = input("Enter your first name: ")
-    is_valid_first_name(first_name)
+    last_name = input("Enter your last name: ")
+    
+    valid_first_name = is_valid_name(first_name,"first")
+    valid_last_name = is_valid_name(last_name,"Last")
+    
+    if valid_first_name and valid_last_name:
+        logger.info(f"User successfully registered with First Name: {first_name} and Last Name: {last_name}")
+    else:
+        logger.error(f"Registration failed due to invalid inputs.")    
+
+
 if __name__ == "__main__":
    main()
 
